2ef7f07e4177a337f7007c27832416992df5385d
Andrey Shevandrin <shevandrin@gmail.com>
2022-06-15 01:08:14 +0200

    2

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for, request, redirect
 import requests
+import os
+
+os.environ["GIT_PYTHON_REFRESH"] = "quiet"
+import git
 
 app = Flask(__name__)
 
@@ -31,9 +35,20 @@ def registration():
         return render_template('registration.html')
 
 
-@app.route('/user/<string:name>/<int:id>',)
+@app.route('/user/<string:name>/<int:id>')
 def user(name, id):
     return 'user page ' + name + ' - ' + str(id)
+
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('tucproject/frontendFileStorage.git')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 
 if __name__ == '__main__':
